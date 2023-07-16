9b3ea17384f1eebe2dd77dd3a4ac323886b43707
Dalay20 <137733175+Dalay20@users.noreply.github.com>
2023-07-16 16:16:18 -0500

    Update Proyecto.java

---
+++ Proyecto/src/main/java/ec/edu/espol/proyecto/Proyecto.java
@@ -0,0 +1,12 @@
+package ec.edu.espol.proyecto;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+public class Proyecto {
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        Menu.iniciarMenu();
+  
+    }    
+}
+
